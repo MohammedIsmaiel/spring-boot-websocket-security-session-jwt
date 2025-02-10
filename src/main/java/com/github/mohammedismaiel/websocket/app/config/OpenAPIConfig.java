865cd7f0d6a37b485d32585a833c8de88d116c96
mohammedismaiel <mohammedismaiel333.z@gmail.com>
2025-02-10 16:53:21 +0200

    config swagger ui to send bearer for testing

---
+++ src/main/java/com/github/mohammedismaiel/websocket/app/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.github.mohammedismaiel.websocket.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "GeoTacking APIs", version = "1.0"))
+public class OpenAPIConfig {
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
